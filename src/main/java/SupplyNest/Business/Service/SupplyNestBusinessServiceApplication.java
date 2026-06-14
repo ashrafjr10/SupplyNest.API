f0a82f60f860c46eb711e5b2de918f565e852c3b
AshrafJamadar10 <ashraf_bj.championxi@outlook.com>
2026-06-14 23:02:13 +0530

    Initial commit

---
+++ src/main/java/SupplyNest/Business/Service/SupplyNestBusinessServiceApplication.java
@@ -0,0 +1,13 @@
+package SupplyNest.Business.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyNestBusinessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyNestBusinessServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SupplyNest/Business/Service/SupplyNestBusinessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package SupplyNest.Business.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyNestBusinessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
